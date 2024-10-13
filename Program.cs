823d91a7b5ff1afea3eafeb17fdd7b52f0a522c2
kubrakhmd <kubraxe-bp217@code.edu.az>
2024-10-13 18:49:55 +0400

    task added

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+
+namespace task_14._10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            Student student = new Student("Kubra", "Ahmadova", "TT-02", 80);
+            Student student2 = new Student("Fatima", "Karimli", "RM-120", 50);
+            student.GetInfo();
+            student.GetDiplomDegree(); 
+            student.CheckGraduation();
+            student2.GetInfo();
+            student2.CheckGraduation();
+            student2.GetDiplomDegree();
+
+        }
+    }
+}
---
+++ student .cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace task_14._10
+{
+    internal class Student
+    {
+        public string Name;
+        public string Surname;
+        public string Group;
+        public byte Point;
+        public bool IsGraduated;
+
+        public Student(string name, string surname, string group, byte point)
+        {
+            Name = name;
+            Surname = surname;
+            Group = group;
+            Point = point;
+            CheckGraduation();
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Surname: {Surname}, Group: {Group}, Point: {Point}, IsGraduated: {IsGraduated}");
+        }
+        public void CheckGraduation()
+        {
+            IsGraduated = Point >= 65; 
+
+            if (IsGraduated)
+            {
+                Console.WriteLine("Mezun olub");
+            }
+            else
+            {
+                Console.WriteLine("Kesilib");
+            }
+        }
+
+
+
+        public void GetDiplomDegree()
+        {
+            if (Point < 65)
+            {
+                Console.WriteLine("Yoxdur");
+            }
+            else if (Point <= 80)
+            {
+                Console.WriteLine("Adi");
+            }
+            else if (Point <= 90)
+            {
+                Console.WriteLine("Şeref");
+            }
+            else // Point > 90
+            {
+                Console.WriteLine("Yüksək şərəf");
+            }
+        }
+    }
+}
+
+
+    
